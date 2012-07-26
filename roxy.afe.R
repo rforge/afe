f361d90fde557e94ca6ccfada4cedfad8ffda407
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-26 15:09:20 +0000

    first commit (not functional)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ roxy.afe.R
@@ -0,0 +1,22 @@
+
+require(roxyPackage)
+
+roxy.package(
+	pck.source.dir = "pkg/afe",
+	pck.version = "0.1-0",
+	pck.description = data.frame(
+		Package = "afe",
+		Type = "Package",
+		Title = "Analysis of Factorial Experiments",
+		Author = "Henrik Singmann <henrik.singmann@psychologie.uni-freiburg.de>",
+		AuthorsR = "c(person(given=\"Henrik\", family=\"Singmann\", email=\"henrik.singmann@psychologie.uni-freiburg.de\", role=c(\"aut\", \"cre\")))",
+		Depends = "R (>= 2.14.0), car",
+		Description = "Provides convenience functions for analyzing factorial experiments, namely provide convenience tools for ANOVA and ANCOVA analyses with type 2 and type 3 sums of squares wrapping car::Anova().",
+		License = "GPL (>=3)",
+		Encoding = "UTF-8",
+		stringsAsFactors = FALSE),
+		actions = "roxy",
+		R.libs = "C:/Program Files/R/R-2.15.1/library", repo.root = "rp.tmp")
+		
+		
+
