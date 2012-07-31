853df38bb33414a741edc8f375025107717f043b
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-31 16:46:00 +0000

    removing inst directory.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/afe/R/afe-package.R
+++ pkg/afe/R/afe-package.R
@@ -3,7 +3,7 @@
 #' \tabular{ll}{
 #' Package: \tab afe\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.1-11\cr
+#' Version: \tab 0.1-12\cr
 #' Date: \tab 2012-07-31\cr
 #' Depends: \tab R (>= 2.14.0), car, reshape2, stringr\cr
 #' Encoding: \tab UTF-8\cr
