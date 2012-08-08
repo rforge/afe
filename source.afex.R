fe3f881dbe2f020504374891c2adbf363f1b1e2e
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-08 12:41:32 +0000

    added source.afex.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ source.afex.R
@@ -0,0 +1,15 @@
+
+require(car)
+require(reshape2)
+require(stringr)
+
+options(contrasts=c('contr.sum','contr.poly')) 
+
+
+for (r.file in list.files("pkg/afex/R", full.names = TRUE)) {
+	source(r.file)
+}
+
+
+options(error = recover)
+options(error = NULL)
