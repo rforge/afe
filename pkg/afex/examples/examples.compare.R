e9baabc92454283507c83aa4a2963321105e4bfd
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-24 00:01:41 +0000

    added further tests to compare.2.vectors() (and cleaned the function)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/afex/examples/examples.compare.R
+++ pkg/afex/examples/examples.compare.R
@@ -2,8 +2,19 @@
 with(sleep, compare.2.vectors(extra[group == 1], extra[group == 2]))
 
 # gives:
-##          test test.statistic test.value  test.df          p
-## 1           t              t  -1.860813 18.00000 0.07918671
-## 2       welch              t  -1.860813 17.77647 0.07939414
-## 3    wilcoxon              W  25.500000       NA 0.06932758
-## A permutation              Z  -1.750807       NA 0.08144796
+## $parametric
+##    test test.statistic test.value test.df       p
+## 1     t              t     -1.861   18.00 0.07919
+## 2 Welch              t     -1.861   17.78 0.07939
+## 
+## $nonparametric
+##              test test.statistic test.value test.df       p
+## 1 stats::Wilcoxon              W     25.500      NA 0.06933
+## 2     permutation              Z     -1.751      NA 0.08154
+## 3  coin::Wilcoxon              Z     -1.854      NA 0.06487
+## 4          median              Z      1.744      NA 0.17867
+
+
+
+# doesn't make much sense as the data is not paired, but whatever:
+with(sleep, compare.2.vectors(extra[group == 1], extra[group == 2], paired = TRUE))
