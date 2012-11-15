5b0900be2cf2b275154b7ab69b4a5759839b901e
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-15 15:40:11 +0000

    added alternative argument to compare.2.vectors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/afex/R/afex-package.R
+++ pkg/afex/R/afex-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab afex\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.4-48\cr
-#' Date: \tab 2012-10-02\cr
+#' Version: \tab 0.4-49\cr
+#' Date: \tab 2012-11-15\cr
 #' Depends: \tab R (>= 2.14.0), coin, car, lme4, pbkrtest (>= 0.3-2), reshape2, stringr\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>=3)\cr
