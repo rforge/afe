295042ac7d9406fb28ea889ae10f49de55ae0fe4
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-22 21:36:44 +0000

    removed ges bug, added Maxwell & Delaney examples.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@93 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/afex/R/afex-package.R
+++ pkg/afex/R/afex-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab afex\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.8-91\cr
-#' Date: \tab 2013-12-18\cr
+#' Version: \tab 0.8-93\cr
+#' Date: \tab 2014-02-22\cr
 #' Depends: \tab R (>= 2.14.0), coin, car, lme4 (>= 1.0.4), pbkrtest (>= 0.3-6)\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>=3)\cr
