ee3a4dbc021376f66b11029488b9abe343d0e6b5
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-08 20:18:15 +0000

    warnings now also propagated nicely in print.mixed when type = 2.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@104 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/afex/R/mixed.R
+++ pkg/afex/R/mixed.R
@@ -365,8 +365,13 @@ print.mixed <- function(x, ...) {
     tmp <- x[[1]]
   }
   tmp[,"p.value"] <- round_ps(tmp[,"p.value"])
-  warnings1 <- c(full = list(x$full.model@optinfo$warnings), lapply(x[[3]], function(y) y@optinfo$warnings))  
-  warnings2 <- c(full = list(ntry(x$full.model@optinfo$conv$lme4$messages)), lapply(x[[3]], function(y) ntry(y@optinfo$conv$lme4$messages)))  
+  if (is.list(x$full)) {
+    warnings1 <- c(full = lapply(x[[2]], function(y) y@optinfo$warnings), lapply(x[[3]], function(y) y@optinfo$warnings))  
+    warnings2 <- c(full = lapply(x[[2]], function(y) ntry(y@optinfo$conv$lme4$messages)), lapply(x[[3]], function(y) ntry(y@optinfo$conv$lme4$messages)))
+  } else {
+    warnings1 <- c(full = list(x$full.model@optinfo$warnings), lapply(x[[3]], function(y) y@optinfo$warnings))  
+    warnings2 <- c(full = list(ntry(x$full.model@optinfo$conv$lme4$messages)), lapply(x[[3]], function(y) ntry(y@optinfo$conv$lme4$messages)))
+  }
   warnings <- mapply(function(x, y) c(unlist(x), y), warnings1, warnings2, SIMPLIFY=FALSE)  
   warn <- vapply(warnings, function(y) !length(y)==0, NA)
   for (i in names(warn)[warn]) warning("lme4 reported (at least) the following warnings for '", i, "':\n  * ", paste(warnings[[i]], collapse = "\n  * "))
