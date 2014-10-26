a5385673997b0370ba39d6c8f38eaa3b555fe942
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-26 08:45:25 +0000

    fixed bug in mixed(..., mehtod="PB") when having only one fixed effect.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@134 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/afex/R/afex-package.R
+++ pkg/afex/R/afex-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab afex\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.12-133\cr
-#' Date: \tab 2014-10-20\cr
+#' Version: \tab 0.12-134\cr
+#' Date: \tab 2014-10-26\cr
 #' Depends: \tab R (>= 3.0.0), car, lme4 (>= 1.0.5), reshape2\cr
 #' Encoding: \tab latin1\cr
 #' License: \tab GPL (>=3)\cr
