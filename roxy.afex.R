eb912edd106836e9af9d6990f1f449d97ae1e12f
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-11 23:22:41 +0000

    updated author list and version bump
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@137 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- roxy.afex.R
+++ roxy.afex.R
@@ -12,13 +12,14 @@ svn.number <- as.numeric(substr(system("svn info", intern=TRUE)[6], 10, 15)) +1
 
 roxy.package(
 	pck.source.dir = "pkg/afex",
-	pck.version = str_c("0.12-", svn.number),
+	pck.version = str_c("0.13-", svn.number),
 	pck.description = data.frame(
 		Package = "afex",
 		Type = "Package",
 		Title = "Analysis of Factorial Experiments",
 		AuthorsR = "c(person(given=\"Henrik\", family=\"Singmann\", email=\"singmann+afex@gmail.com\", role=c(\"aut\", \"cre\")),
         person(given=\"Ben\", family=\"Bolker\", role=c(\"aut\")),
+        person(given=\"Jake\", family=\"Westfall\", role=c(\"aut\")),
         person(given=\"Søren\", family=\"Højsgaard\", role=c(\"ctb\")),
         person(given=\"John\", family=\"Fox\", role=c(\"ctb\")),
         person(given=\"Michael A.\", family=\"Lawrence\", role=c(\"ctb\")),
@@ -27,7 +28,7 @@ roxy.package(
 		Depends = "R (>= 3.0.0), car, lme4 (>= 1.0.5), reshape2",
 		Suggests = "ascii, xtable, parallel, plyr, optimx, nloptr",
         Imports = "stringr, coin, Matrix, pbkrtest (>= 0.3-6)",
-		Description = "Provides convenience functions for analyzing factorial experiments using ANOVA or mixed models. ez.glm(), aov.car(), or aov4() allow convenient specification of between, within (i.e., repeated-measures), or mixed between-within (i.e., split-plot) ANOVAs for data in long format (i.e., one observation per row) aggregating more then one observation per individual and cell of the design. mixed() fits a mixed model using lme4::lmer() and computes p-values for all effects in the model using either Kenward-Roger approximation of degrees of freedom (LMM only), parametric bootstrap (LMMs and GLMMs) or likelihood ratio tests (LMMs and GLMMs). afex uses type 3 sums of squares as default (imitating commercial statistical software). compare.2.vectors() compares two vectors using a variety of tests (t, wilcoxon, and permutation).",
+		Description = "Provides convenience functions for analyzing factorial experiments using ANOVA or mixed models. ez.glm(), aov.car(), or aov4() allow convenient specification of between, within (i.e., repeated-measures), or mixed between-within (i.e., split-plot) ANOVAs for data in long format (i.e., one observation per row) aggregating more then one observation per individual and cell of the design. mixed() fits a mixed model using lme4::lmer() and computes p-values for all effects in the model using either Kenward-Roger approximation of degrees of freedom (LMM only), parametric bootstrap (LMMs and GLMMs), or likelihood ratio tests (LMMs and GLMMs). afex uses type 3 sums of squares as default (imitating commercial statistical software). compare.2.vectors() compares two vectors using a variety of tests (t, wilcoxon, and permutation).",
 		URL = "http://www.psychologie.uni-freiburg.de/Members/singmann/R/afex",
 		License = "GPL (>=3)",
 		Encoding = "latin1",
