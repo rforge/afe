49566cdb7e570b28f6b50073d9c8d2628b8692d4
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-07 19:39:04 +0000

    updated lsmeans requirement to 2.17
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/afe@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/afex/R/afex-package.R
+++ pkg/afex/R/afex-package.R
@@ -3,15 +3,15 @@
 #' \tabular{ll}{
 #' Package: \tab afex\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.14-169\cr
-#' Date: \tab 2015-04-21\cr
-#' Depends: \tab R (>= 3.0.0), lme4 (>= 1.0.5), reshape2, lsmeans (>= 2.10)\cr
+#' Version: \tab 0.14-170\cr
+#' Date: \tab 2015-05-07\cr
+#' Depends: \tab R (>= 3.0.0), lme4 (>= 1.0.5), reshape2, lsmeans (>= 2.17)\cr
 #' Encoding: \tab latin1\cr
 #' License: \tab GPL (>=3)\cr
 #' URL: \tab http://www.psychologie.uni-freiburg.de/Members/singmann/R/afex\cr
 #' }
 #'
-#' Provides convenience functions for analyzing factorial experiments using ANOVA or mixed models. ez.glm(), aov.car(), and aov4() allow convenient specification of between, within (i.e., repeated-measures), or mixed between-within (i.e., split-plot) ANOVAs for data in long format (i.e., one observation per row), potentially aggregating multiple observations per individual and cell of the design. mixed() fits a mixed model using lme4::lmer() and computes p-values for all effects using either Kenward-Roger approximation for degrees of freedom (LMM only), parametric bootstrap (LMMs and GLMMs), or likelihood ratio tests (LMMs and GLMMs). afex uses type 3 sums of squares as default (imitating commercial statistical software).
+#' Provides convenience functions for analyzing factorial experiments using ANOVA or mixed models. ez.glm(), aov.car(), and aov4() allow convenient specification of between, within (i.e., repeated-measures), or mixed between-within (i.e., split-plot) ANOVAs for data in long format (i.e., one observation per row), potentially aggregating multiple observations per individual and cell of the design. mixed() fits mixed models using lme4::lmer() and computes p-values for all effects using either Kenward-Roger approximation for degrees of freedom (LMM only), parametric bootstrap (LMMs and GLMMs), or likelihood ratio tests (LMMs and GLMMs). afex uses type 3 sums of squares as default (imitating commercial statistical software).
 #'
 #' @aliases afex-package
 #' @name afex-package
